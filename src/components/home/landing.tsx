1fd121abc3ea62ef74166d35a99fd72ca273048c
Kent Jordan <kentaki.dev@gmail.com>
2023-11-15 19:33:17 +0800

    Finally fixed my image overflow

--- src/components/home/landing.tsx
+++ src/components/home/landing.tsx
@@ -18,13 +18,13 @@ const HomeLanding = () => {
 
   return (
     <div
-      className={`flex justify-center flex-wrap relative h-screen`}
+      className={`flex justify-center items-end flex-wrap relative h-screen `}
       style={{
         maxHeight: `${height}px`,
       }}>
       <div className='absolute bg-gradient-to-t from-black via-black/10  to-transparent max-w-[1280px] w-full h-full'></div>
       {/* Info about me */}
-      <div className='absolute bottom-0 text-center xl:text-start flex-col justify-center flex-[.5] xl:flex xl:relative'>
+      <div className='absolute bottom-0 flex text-center xl:text-start flex-col h-full flex-[0.5] justify-end xl:justify-center xl:flex xl:relative'>
         <span
           className={`${roboto.className} block my-2 text-xl sm:text-4xl text-stone-300`}>
           Hello, I&apos;m
@@ -58,11 +58,8 @@ const HomeLanding = () => {
         </div>
       </div>
       {/*  Image of me*/}
-      <div className='flex-col justify-end items-center flex-1 flex overflow-hidden'>
-        <Image
-          src={Me}
-          className='min-w-[800px] w-[800px] h-[680px]'
-          alt='Me'></Image>
+      <div className='flex-col justify-end items-center flex-1 flex overflow-hidden h-full'>
+        <img src={Me.src} className='h-[80%] object-cover ' alt='Me'></img>
       </div>
     </div>
   );
