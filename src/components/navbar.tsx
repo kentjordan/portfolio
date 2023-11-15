dfce6863c534a2477e0ad1c5a3c143d133ac5108
Kent Jordan <kentaki.dev@gmail.com>
2023-11-15 19:33:33 +0800

    Added gradient background

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,14 +7,14 @@ const roboto = Roboto({ weight: ["500", "700"], subsets: ["latin"] });
 function Navbar() {
   return (
     <div
-      className={`${roboto.className} hidden md:flex justify-between max-w-[1280px] w-full absolute z-50 m-8 text-lg text-stone-300 px-8`}>
-      <div className='flex items-center gap-4'>
+      className={`${roboto.className}  bg-gradient-to-b from-black via-black/50 to-transparent hidden md:flex justify-between max-w-[1280px] w-full absolute z-50 mx-8 text-lg text-stone-300 p-8`}>
+      <div className='flex items-center text-base gap-4'>
         <BsDownload size={20} />
         <h1 className={`${roboto.className} cursor-pointer font-extrabold`}>
           Get Resume/CV
         </h1>
       </div>
-      <ul className='flex gap-8'>
+      <ul className='flex gap-8 text-base'>
         <li className='cursor-pointer'>
           <Link href='/'>Home</Link>
         </li>
