c7dc33f1ce57764db563a1d9b5838a071c5b3de3
Kent Jordan <kentaki.dev@gmail.com>
2024-03-10 15:45:16 +0800

    Updated type

--- src/types/works.d.ts
+++ src/types/works.d.ts
@@ -1,9 +1,10 @@
+import { StaticImageData } from "next/image";
 
 export interface WorkItem {
     id: string,
     title: string,
     description: string,
-    img_url: Array<string>,
+    img_url: Array<string | StaticImageData>,
     view_url: string,
     isOnDevelopment: boolean,
     tags: Array<string>,
