062d9ba20b21ffb64f25ceb845ccece98bfd4371
Kent Jordan <kentaki.dev@gmail.com>
2024-03-10 15:51:06 +0800

    Added space

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ const ContactPage = () => {
     <div className='w-full max-w-[1280px]'>
       <div className='flex flex-col justify-center  h-screen'>
         <h1 className='text-3xl text-center px-4 break-words'>
-          Contact me via email at
+          Contact me via email at{" "}
           <b className='text-blue-300'>job.kentjordan@gmail.com</b>
         </h1>
       </div>
