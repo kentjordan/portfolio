ef70a993466e3dd79affbfffcf6cbeea66eb430a
Kent Jordan <kentaki.dev@gmail.com>
2024-04-17 20:03:24 +0800

    Updated UI

--- src/components/home/technologies.tsx
+++ src/components/home/technologies.tsx
@@ -1,34 +1,140 @@
+"use client";
 import {
   ReactOriginalWordmark,
   TypescriptOriginal,
-  ExpressOriginal,
   PostgresqlOriginal,
-  TailwindcssPlain,
+  TailwindcssOriginal,
   PythonOriginal,
-  FastapiOriginalWordmark,
   DockerOriginal,
   UbuntuPlainWordmark,
   NginxOriginal,
   NextjsOriginalWordmark,
   AmazonwebservicesPlainWordmark,
+  TensorflowOriginal,
+  PandasOriginal,
+  NumpyOriginal,
+  ScikitlearnOriginal,
+  NestjsOriginal,
+  JupyterOriginalWordmark,
 } from "devicons-react";
+import { LinearGradient } from "react-text-gradients";
+
+import { Anton } from "next/font/google";
+
+const anton = Anton({ weight: "400", subsets: ["latin"] });
 
 function HomeTechnologies() {
   return (
-    <div className='w-full bg-stone-50 my-80'>
-      <div className='flex gap-6 justify-center items-center p-10 m-4 flex-wrap'>
-        <TypescriptOriginal size={64} />
-        <ReactOriginalWordmark size={64} />
-        <NextjsOriginalWordmark size={80} />
-        <TailwindcssPlain size={64} />
-        <ExpressOriginal size={64} />
-        <PythonOriginal size={64} />
-        <FastapiOriginalWordmark size={120} />
-        <PostgresqlOriginal size={64} />
-        <UbuntuPlainWordmark size={64} />
-        <DockerOriginal size={64} />
-        <NginxOriginal size={64} />
-        <AmazonwebservicesPlainWordmark size={64} />
+    <div className='w-full min-h-screen flex flex-col justify-center items-center rounded text-black p-2 my-64'>
+      {/* Header */}
+      <div className='flex justify-center items-center flex-col mb-24'>
+        <h1 className={`${anton.className} text-6xl my-8 text-center`}>
+          <LinearGradient
+            gradient={["to left", "#0ED2F7, #B2FEFA"]}
+            fallbackColor='#6DD5FA'>
+            Techstack
+          </LinearGradient>
+        </h1>
+        <h1 className='text-stone-200 text-center'>
+          The technologies that I have been working with
+        </h1>
+      </div>
+      <div className='bg-white rounded-md'>
+        {/* Web */}
+        <div className='flex flex-col items-center p-2 mt-8 mb-12'>
+          <h1 className='text-lg mb-4 text-center font-bold'>
+            Fullstack web and app
+          </h1>
+          <div className='flex items-center justify-center gap-8 max-w-[60%] my-4 flex-wrap'>
+            <TypescriptOriginal
+              size={64}
+              fill='currentColor'
+              className='text-white'
+            />
+            <ReactOriginalWordmark
+              size={64}
+              fill='currentColor'
+              className='text-white'
+            />
+            <DockerOriginal
+              size={64}
+              fill='currentColor'
+              className='text-white'
+            />
+
+            <NestjsOriginal
+              size={64}
+              fill='currentColor'
+              className='text-white'
+            />
+            <TailwindcssOriginal
+              size={64}
+              fill='currentColor'
+              className='text-white'
+            />
+            {/* <ExpressOriginal size={40}  fill='currentColor' className='text-white' /> */}
+            {/* <FastapiOriginalWordmark size={120} fill='currentColor' className='text-white' /> */}
+            <PostgresqlOriginal
+              size={64}
+              fill='currentColor'
+              className='text-white'
+            />
+            <NextjsOriginalWordmark size={80} />
+            <NginxOriginal
+              size={80}
+              fill='currentColor'
+              className='text-white'
+            />
+            <UbuntuPlainWordmark
+              size={96}
+              fill='currentColor'
+              className='text-white'
+            />
+            <AmazonwebservicesPlainWordmark
+              size={64}
+              fill='currentColor'
+              className='text-white'
+            />
+          </div>
+        </div>
+        {/* Machine Learing */}
+        <div className='flex flex-col items-center p-2 mt-8 mb-12'>
+          <h1 className='text-lg mb-8 text-center font-bold'>
+            Machine Learning
+          </h1>
+          <div className='flex items-center my-4 justify-center gap-8 max-w-[100%] flex-wrap'>
+            <PythonOriginal
+              size={64}
+              fill='currentColor'
+              className='text-white'
+            />
+            <JupyterOriginalWordmark
+              size={64}
+              fill='currentColor'
+              className='text-white'
+            />
+            <TensorflowOriginal
+              size={64}
+              fill='currentColor'
+              className='text-white'
+            />
+            <PandasOriginal
+              size={64}
+              fill='currentColor'
+              className='text-white'
+            />
+            <NumpyOriginal
+              size={64}
+              fill='currentColor'
+              className='text-white'
+            />
+            <ScikitlearnOriginal
+              size={64}
+              fill='currentColor'
+              className='text-white'
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
