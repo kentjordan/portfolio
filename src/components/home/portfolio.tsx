b8a9b1bbd190cef62bcb2249b65570fa3cd6933d
Kent Jordan <kentaki.dev@gmail.com>
2024-04-17 20:02:52 +0800

    Updated UI

--- src/components/home/portfolio.tsx
+++ src/components/home/portfolio.tsx
@@ -10,12 +10,13 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
 } from "@/components/ui/carousel";
 import Item from "./item";
 import useFromClient from "@/models/useFromClients";
 import usePersonalProjects from "@/models/usePersonalProjects";
+
+import Autoplay from "embla-carousel-autoplay";
+import { LinearGradient } from "react-text-gradients";
 
 function HomePortfolio() {
   const fromClient = useFromClient();
@@ -23,33 +24,41 @@ function HomePortfolio() {
 
   return (
     <div
-      className='flex flex-col items-center p-4 overflow-hidden xl:overflow-visible '
+      className='flex flex-col items-center p-4 overflow-hidden xl:overflow-visible'
       id='works'>
       <h1 className={`${anton.className} text-6xl my-8 text-center`}>
-        My recent works
+        <LinearGradient
+          gradient={["to left", "#0ED2F7, #B2FEFA"]}
+          fallbackColor='#6DD5FA'>
+          My recent works
+        </LinearGradient>
       </h1>
       <div className='flex flex-col my-12 items-center justify-center mx-12'>
-        <h1 className={`${roboto.className} text-2xl text-center`}>
-          From a client
+        <h1
+          className={`${roboto.className} text-2xl text-center font-bold italic`}>
+          FROM CLIENTS
         </h1>
-        <div className='flex items-center gap-3 justify-center mt-8 text-stone-400 xl:hidden'>
+        <div className='flex items-center gap-3 justify-center mt-8 text-stone-400'>
           <PiHandSwipeLeftLight size={32} className=' text-stone-400' />
           <span>Swipe to see more</span>
         </div>
         <Carousel
+          className='mt-8 p-1 max-w-5xl'
           orientation='horizontal'
+          plugins={[Autoplay({ delay: 1500 })]}
           opts={{
             align: "center",
+            loop: true,
           }}>
           <CarouselContent>
             {fromClient.map((e, i) => {
               return (
                 <CarouselItem
-                  key={i * 3}
-                  className='md:basis-1/2 lg:basis-1/3 flex justify-center'>
+                  key={e.id}
+                  className='lg:basis-96 flex justify-center'>
                   <Item
                     id={e.id}
-                    key={i}
+                    key={e.id}
                     title={e.title}
                     description={e.description}
                     img_url={e.img_url[0]}
@@ -64,28 +73,30 @@ function HomePortfolio() {
         </Carousel>
       </div>
       <div className='flex flex-col my-12 items-center justify-center mx-12'>
-        <h1 className={`${roboto.className} text-2xl text-center`}>
-          Personal Projects
+        <h1 className={`${roboto.className} text-2xl text-center font-bold`}>
+          PERSONAL PROJECTS
         </h1>
-        <div className='flex items-center gap-3 justify-center mt-8 text-stone-400 xl:hidden'>
+        <div className='flex items-center gap-3 justify-center mt-8 text-stone-400'>
           <PiHandSwipeLeftLight size={32} className=' text-stone-400' />
           <span>Swipe to see more</span>
         </div>
         <Carousel
+          className='mt-8 p-1 max-w-5xl'
           orientation='horizontal'
+          plugins={[Autoplay({ delay: 1500 })]}
           opts={{
             align: "center",
+            loop: true,
           }}>
-          <CarouselPrevious />
           <CarouselContent>
             {personalProjects.map((e, i) => {
               return (
                 <CarouselItem
-                  key={i * 4}
-                  className='lg:basis-1/3 flex justify-center'>
+                  key={e.id}
+                  className='lg:basis-96 flex justify-center'>
                   <Item
                     id={e.id}
-                    key={i * 2}
+                    key={e.id}
                     title={e.title}
                     description={e.description}
                     img_url={e.img_url[0]}
@@ -97,7 +108,6 @@ function HomePortfolio() {
               );
             })}
           </CarouselContent>
-          <CarouselNext />
         </Carousel>
       </div>
     </div>
