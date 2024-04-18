1a46561ece423fe6cab016684bd937d0f096f720
Kent Jordan <kentaki.dev@gmail.com>
2024-04-18 16:41:51 +0800

    Added: sent personal project

--- src/models/usePersonalProjects.ts
+++ src/models/usePersonalProjects.ts
@@ -10,6 +10,19 @@ import BinarySearch from "@/assets/binary_search.png";
 import LinearSearch from "@/assets/linear_search.png";
 import { WorkItem } from "@/types/works";
 
+import Sent1 from "@/assets/sent/1.png"
+import Sent2 from "@/assets/sent/2.png"
+import Sent3 from "@/assets/sent/3.png"
+import Sent4 from "@/assets/sent/4.png"
+import Sent5 from "@/assets/sent/5.png"
+import Sent6 from "@/assets/sent/6.png"
+import Sent7 from "@/assets/sent/7.png"
+import Sent8 from "@/assets/sent/8.png"
+import Sent9 from "@/assets/sent/9.png"
+import Sent10 from "@/assets/sent/10.png"
+import Sent11 from "@/assets/sent/11.png"
+import Sent12 from "@/assets/sent/12.png"
+
 const usePersonalProjects = () => {
 
     const [personalProjects, setPersonalProjects] = useState<WorkItem[]>([
@@ -31,6 +44,31 @@ const usePersonalProjects = () => {
             isOnDevelopment: false,
             tags: ["REACT.JS"],
         },
+        {
+            id: "PP-011",
+            title: "Sent",
+            description: "A social media with a real-time messaging system",
+            body: "In building of this personal project, I used NEXT.js and TailwindCSS in the frontend while in the backend I used NestJS and PostgreSQL.",
+            tech_stack: ["NEXT.js", "PostgreSQL", "NestJS"],
+            img_url: [
+                Sent1,
+                Sent2,
+                Sent3,
+                Sent4,
+                Sent5,
+                Sent6,
+                Sent7,
+                Sent8,
+                Sent9,
+                Sent10,
+                Sent11,
+                Sent12,
+            ],
+            view_url: "https://kabsahi-devlopment.vercel.app/",
+            isOnDevelopment: false,
+            tags: ["NEXT.JS", "POSTGRE-SQL", "NEST.JS"],
+        }
+        ,
         {
             id: "PP-002",
             title: "Calculator",
@@ -54,18 +92,18 @@ const usePersonalProjects = () => {
             isOnDevelopment: false,
             tags: ["REACT.JS"],
         },
-        {
-            id: "PP-004",
-            title: "Linear Search Visualizer",
-            tech_stack: ["React.js"],
-            description: "To visualize the linear search algorithm.",
-            body: "This simple visualizer of the the Linear Search algorithm is written in React.js.",
-            img_url: [LinearSearch],
-            view_url:
-                "https://kjordan-visualize.vercel.app/algorithms/searching/linear-search/demo",
-            isOnDevelopment: false,
-            tags: ["REACT.JS"],
-        },
+        // {
+        //     id: "PP-004",
+        //     title: "Linear Search Visualizer",
+        //     tech_stack: ["React.js"],
+        //     description: "To visualize the linear search algorithm.",
+        //     body: "This simple visualizer of the the Linear Search algorithm is written in React.js.",
+        //     img_url: [LinearSearch],
+        //     view_url:
+        //         "https://kjordan-visualize.vercel.app/algorithms/searching/linear-search/demo",
+        //     isOnDevelopment: false,
+        //     tags: ["REACT.JS"],
+        // },
     ]);
 
     return personalProjects;
