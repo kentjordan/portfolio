f45356932d52c6fe710ce15f02223da9770f24b7
Kent Jordan <kentaki.dev@gmail.com>
2024-06-13 20:04:30 +0800

    fix: typo in email

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,7 @@ const ContactPage = () => {
       <div className='flex flex-col justify-center  h-screen'>
         <h1 className='text-3xl text-center px-4 break-words'>
           Contact me via email at{" "}
-          <b className='text-blue-300'>job.kentjordan@gmail.com</b>
+          <b className='text-blue-300'>job.kjordan@gmail.com</b>
         </h1>
       </div>
     </div>
