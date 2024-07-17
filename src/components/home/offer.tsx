a7d387ceefe02f91d779338085293e001638e86a
Kent Jordan <kentaki.dev@gmail.com>
2024-07-17 09:35:42 +0800

    decreased text and height sizes

--- src/components/home/offer.tsx
+++ src/components/home/offer.tsx
@@ -31,20 +31,17 @@ const HomeOffer = () => {
           . 😉
         </h1>
       </div>
-      <div className='w-full flex mt-16 gap-4 flex-wrap justify-center'>
+      <div className='w-full flex mt-8 gap-4 flex-wrap justify-center'>
         {/* Websites */}
-        <div className='w-[330px] h-[420px] border-neutral-800 border bg-neutral-950 rounded-md p-8 group hover:border-sky-300'>
+        <div className='w-[300px] h-[380px] border-neutral-800 border bg-neutral-950 rounded-md p-6 group hover:border-sky-300'>
           <div className='flex items-center gap-4'>
-            <PanelTop
-              className='text-white group-hover:text-sky-300'
-              size={32}
-            />
+            <PanelTop className='text-white ' size={32} />
             <h1
-              className={`${anton.className} text-white text-3xl font-bold group-hover:text-sky-300 cursor-default`}>
+              className={`${anton.className} text-white text-xl font-bold  cursor-default`}>
               WEBSITES
             </h1>
           </div>
-          <p className='text-neutral-400 text-lg leading-8 mt-8 group-hover:text-white cursor-default'>
+          <p className='text-neutral-400 text-base leading-8 mt-8 group-hover:text-white cursor-default'>
             Got an idea in mind? or do you want to increase the presence of your
             current business? I can help you with that. I develop static
             websites. Let’s publish your business’s brand or profile on the
@@ -52,48 +49,45 @@ const HomeOffer = () => {
           </p>
         </div>
         {/* Web Apps */}
-        <div className='w-[330px] h-[420px] border-neutral-800 border bg-neutral-950 rounded-md p-8 group hover:border-sky-300'>
+        <div className='w-[300px] h-[380px] border-neutral-800 border bg-neutral-950 rounded-md p-6 group hover:border-sky-300'>
           <div className='flex items-center gap-4'>
-            <Globe className='text-white group-hover:text-sky-300' size={32} />
+            <Globe className='text-white ' size={32} />
             <h1
-              className={`${anton.className} text-white text-3xl font-bold group-hover:text-sky-300 cursor-default`}>
+              className={`${anton.className} text-white text-xl font-bold  cursor-default`}>
               WEB APPS
             </h1>
           </div>
-          <p className='text-neutral-400 text-lg leading-8 mt-8 group-hover:text-white cursor-default'>
+          <p className='text-neutral-400 text-base leading-8 mt-8 group-hover:text-white cursor-default'>
             Beyond just a static website, I can help you in your complex
             business problem by creating a fully functional web-based system to
             fulfill its needs. 🛠️
           </p>
         </div>
         {/* Mobile Apps */}
-        <div className='w-[330px] h-[420px] border-neutral-800 border bg-neutral-950 rounded-md p-8 group hover:border-sky-300'>
+        <div className='w-[300px] h-[380px] border-neutral-800 border bg-neutral-950 rounded-md p-6 group hover:border-sky-300'>
           <div className='flex items-center gap-4'>
-            <Smartphone
-              className='text-white group-hover:text-sky-300'
-              size={32}
-            />
+            <Smartphone className='text-white ' size={32} />
             <h1
-              className={`${anton.className} text-white text-3xl font-bold group-hover:text-sky-300 cursor-default`}>
+              className={`${anton.className} text-white text-xl font-bold  cursor-default`}>
               MOBILE APPS
             </h1>
           </div>
-          <p className='text-neutral-400 text-lg leading-8 mt-8 group-hover:text-white cursor-default'>
+          <p className='text-neutral-400 text-base leading-8 mt-8 group-hover:text-white cursor-default'>
             If mobile phone users are your target audience or you just want an
             app for your business or problem, I can help and provide you a high
             performant and cross-platform mobile app. 📱
           </p>
         </div>
         {/* ML/AI */}
-        <div className='w-[330px] h-[420px] border-neutral-800 border bg-neutral-950 rounded-md p-8 group hover:border-sky-300'>
+        <div className='w-[300px] h-[380px] border-neutral-800 border bg-neutral-950 rounded-md p-6 group hover:border-sky-300'>
           <div className='flex items-center gap-4'>
-            <Share2 className='text-white group-hover:text-sky-300' size={32} />
+            <Share2 className='text-white ' size={32} />
             <h1
-              className={`${anton.className} text-white text-3xl font-bold group-hover:text-sky-300 cursor-default`}>
+              className={`${anton.className} text-white text-xl font-bold  cursor-default`}>
               ML/AI
             </h1>
           </div>
-          <p className='text-neutral-400 text-lg leading-8 mt-8 group-hover:text-white cursor-default'>
+          <p className='text-neutral-400 text-base leading-8 mt-8 group-hover:text-white cursor-default'>
             I’m still improving my skills in Machine Learning and Artificial
             Intelligence. I’ll help you soon once I got comfortable in this
             field. 😉
