b08bf4a2a6b2326bbb8ce18c4da05d57d592e652
Kent Jordan <kentaki.dev@gmail.com>
2024-07-18 09:44:27 +0800

    fix: footer items onclick area

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,39 +5,37 @@ const Footer = () => {
   return (
     <div className=' w-full flex flex-col items-center pb-8 border-t border-neutral-800 mt-32'>
       <ul className='flex gap-8 justify-center my-16 flex-wrap'>
-        <li className='cursor-pointer hover:border-b-sky-500 hover:border-b py-1 px-2'>
-          <Link className='text-sm' href='/'>
-            Home
-          </Link>
-        </li>
-        <li className='cursor-pointer hover:border-b-sky-500 hover:border-b py-1 px-2'>
-          <Link className='text-sm' href='/#techstack'>
-            Techstack
-          </Link>
-        </li>
-        <li className='cursor-pointer hover:border-b-sky-500 hover:border-b py-1 px-2'>
-          <Link className='text-sm' href='/#works'>
-            Works
-          </Link>
-        </li>
-        {/* <li className='cursor-pointer'>
-          <Link className="text-sm" href='/blogs'>Blogs</Link>
-        </li> */}
-        <li className='cursor-pointer hover:border-b-sky-500 hover:border-b py-1 px-2'>
-          <Link className='text-sm' href='/#certificates'>
-            Certificates
-          </Link>
-        </li>
-        <li className='cursor-pointer hover:border-b-sky-500 hover:border-b px-2 py-1 hover:text-white'>
-          <Link className='text-sm' href='/#about-me'>
-            About Me
-          </Link>
-        </li>
-        <li className='cursor-pointer hover:border-b-sky-500 hover:border-b py-1 px-2'>
-          <Link className='text-sm' href='/contact'>
-            Contact
-          </Link>
-        </li>
+        <Link
+          className='text-sm cursor-pointer hover:border-b-sky-500 hover:border-b py-1 px-2'
+          href='/'>
+          Home
+        </Link>
+        <Link
+          className='text-sm cursor-pointer hover:border-b-sky-500 hover:border-b py-1 px-2'
+          href='/#techstack'>
+          Techstack
+        </Link>
+        <Link
+          className='text-sm cursor-pointer hover:border-b-sky-500 hover:border-b py-1 px-2'
+          href='/#works'>
+          Works
+        </Link>
+        {/* <Link className="text-sm cursor-pointer hover:border-b-sky-500 hover:border-b py-1 px-2" href='/blogs'>Blogs</Link> */}
+        <Link
+          className='text-sm cursor-pointer hover:border-b-sky-500 hover:border-b py-1 px-2'
+          href='/#certificates'>
+          Certificates
+        </Link>
+        <Link
+          className='text-sm cursor-pointer hover:border-b-sky-500 hover:border-b py-1 px-2'
+          href='/#about-me'>
+          About Me
+        </Link>
+        <Link
+          className='text-sm cursor-pointer hover:border-b-sky-500 hover:border-b py-1 px-2'
+          href='/contact'>
+          Contact
+        </Link>
       </ul>
       <h1 className='text-sm text-center'>
         Copyright {new Date().getFullYear()} | Kent John Jordan
