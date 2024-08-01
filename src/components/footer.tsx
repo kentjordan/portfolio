402299d392779a0f83da50309efb31e31ce83f06
Kent Jordan <kentaki.dev@gmail.com>
2024-08-01 21:33:09 +0800

    feat: updated navbar items layout

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className=' w-full flex flex-col items-center pb-8 border-t border-neutral-800 mt-32'>
-      <ul className='flex gap-8 justify-center my-16 flex-wrap'>
+      <ul className='flex gap-8 justify-center my-16 sm:flex-row pl-4 flex-col w-full'>
         <Link
           className='text-sm cursor-pointer hover:border-b-sky-500 hover:border-b py-1 px-2'
           href='/'>
@@ -37,7 +37,7 @@ const Footer = () => {
           Contact
         </Link>
       </ul>
-      <h1 className='text-sm text-center'>
+      <h1 className='text-sm pl-6 sm:text-center w-full'>
         Copyright {new Date().getFullYear()} | Kent John Jordan
       </h1>
     </div>
